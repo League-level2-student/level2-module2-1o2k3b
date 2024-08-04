3ea9ad56dbb91e3e398a7eae49ee168062489bd6
Aarit Jayaswal <aaritrj@gmail.com>
2024-08-04 11:30:55 -0700

    thatsalotofstagedchanges

---
+++ src/_04_animals_inheritance/Animal.java
@@ -0,0 +1,15 @@
+package _04_animals_inheritance;
+
+class Animal {
+String printName;
+String play;
+String eat;
+String sleep;
+Animal(String printName, String play, String eat, String sleep) {
+
+	this.printName=printName;
+	this.play=play;
+	this.eat=eat;
+	this.sleep=sleep;
+}
+}
--- src/_06_overloading/LeagueOptionPane.java
+++ src/_06_overloading/LeagueOptionPane.java
@@ -3,6 +3,7 @@ package _06_overloading;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,14 +17,27 @@ import javax.swing.JPanel;
  * GOAL: Create your own custom pop-up messages
  */
 public class LeagueOptionPane {
+
 	
 	public static void showMessageDialog(String message) {
 		// 1. Open example.png and make a GUI that looks like that
 		//    The message parameter is what we want to show on our pop-up
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel image = new JLabel();
+		JLabel label = new JLabel();
 		
+		frame.setSize(200, 100);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		panel.add(image);
+		panel.add(label);
+		image.setIcon(loadImage("league.png"));
+		label.setText(message);
 		
 		// 2. Uncomment the line of code below. It sets the location of our frame to the center of the screen
-		//frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null);
 	}
 	
 	// 3. Call this method in the Runner class
